a85d3857acb60cd015d242af8a85e33e066c7eaf
Fahd Mannaa <fahdkuwait@gmail.com>
2022-10-09 17:31:01 +0300

    start from the begining

--- Amigoscode/Amigoscode/src/main/java/org/example/PassByValVsRef.java
+++ Amigoscode/Amigoscode/src/main/java/org/example/PassByValVsRef.java
@@ -1,23 +1,13 @@
 package org.example;
 
-import java.time.LocalDate;
-import java.util.Locale;
 
 public class PassByValVsRef {
     public static void main(String[] args) {
+
+        String name = "fahd";
+        System.out.println(name.);
         
     }
-    public static void main(String[] args){
-        int x = 0;
-        var name = "yehya";
 
-        incrementValue(x);
-        System.out.println(x);
 
-        LocalDate now =
-    }
-
-    static void incrementValue(int value){
-        value++;
-    }
 }
