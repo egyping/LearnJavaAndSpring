5b176c3d89200c68ef6556f4fc1e275fd6839a6f
Fahd Mannaa <fahdkuwait@gmail.com>
2022-10-13 17:25:51 +0300

    until enums

--- AmigoCourse/src/main/java/org/amigoscode/Car.java
+++ AmigoCourse/src/main/java/org/amigoscode/Car.java
@@ -12,6 +12,18 @@ public class Car {
     private int make;
     private boolean sedan;
 
+    // Constructor so you can set the value while the object instantiation
+    public Car(String color, int make, boolean sedan) {
+        this.color = color;
+        this.make = make;
+        this.sedan = sedan;
+    }
+    // we can creat multiple constructor, this constructor allow you to create object with color only and rest are default values
+    public Car(String color){
+        this(color, 1980, false);
+        this.color = color;
+    }
+
     // Set the actual behavior of the model by adding setter and getter
     // ctrl + enter and select setter and getter
     // setter helps yu to call to use the class and set\change the class object
@@ -38,5 +50,15 @@ public class Car {
 
     public void setSedan(boolean sedan) {
         this.sedan = sedan;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", make=" + make +
+                ", sedan=" + sedan +
+                '}';
     }
 }
--- AmigoCourse/src/main/java/org/amigoscode/CarInnAction.java
+++ AmigoCourse/src/main/java/org/amigoscode/CarInnAction.java
@@ -2,11 +2,38 @@ package org.amigoscode;
 
 // This is the class which will make use of the Car class
 // this class must have psvm
+// here we will create the car objects
 public class CarInnAction {
 
     public static void main(String[] args) {
-       Car car = new Car();
-       car.setColor();
+        // create object from the class
+        // bmw is instance of the car class -- instantiation
+       // Car bmw = new Car(); object creation before constructor
+       Car bmw = new Car("White", 2015, true); // create onject after constructor
+       Car toyota = new Car("black");
+       // set and get the object the way you want -- using the model class setter and getter
+       // We used the setter to make it red since the class has no constructor yet
+
+       // default constructor toyota
+        System.out.println("Default constructor");
+        System.out.println(toyota.getColor());
+        System.out.println(toyota.getMake());
+        System.out.println(toyota.isSedan());
+
+        // BMW used the original constructor which you have to define the fields while creating the object
+        System.out.println("Normal constructor which require to mention the fields ");
+        System.out.println(bmw.getColor()); //White
+        System.out.println(bmw.getMake()); //2015
+        System.out.println(bmw.isSedan());
+
+
+        System.out.println("toString to print our the object  ");
+        System.out.println(bmw);
+        // this is without toString at the class org.amigoscode.Car@36baf30c
+        // after adding the toString to the original class
+        // Car{color='White', make=2015, sedan=true}
+
+
 
     }
 }
