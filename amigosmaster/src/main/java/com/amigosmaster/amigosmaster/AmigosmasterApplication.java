1a79acaf0887c2d66dc7dfaaaa6d6ddf7fed10bf
Fahd Mannaa <fahdkuwait@gmail.com>
2022-11-02 17:20:32 +0300

    Amigos master created

---
+++ amigosmaster/src/main/java/com/amigosmaster/amigosmaster/AmigosmasterApplication.java
@@ -0,0 +1,13 @@
+package com.amigosmaster.amigosmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigosmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigosmasterApplication.class, args);
+	}
+
+}
---
+++ amigosmaster/src/test/java/com/amigosmaster/amigosmaster/AmigosmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amigosmaster.amigosmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigosmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
