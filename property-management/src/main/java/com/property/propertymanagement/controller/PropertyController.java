cd5af8a440f488a7aefd1d6701cd2800db68a731
egyping <fahdkuwait@gmail.com>
2022-11-16 20:08:08 +0300

    Working on Entity class and Understanding Different JPA Annotation

--- property-management/src/main/java/com/property/propertymanagement/controller/PropertyController.java
+++ property-management/src/main/java/com/property/propertymanagement/controller/PropertyController.java
@@ -1,11 +1,17 @@
 package com.property.propertymanagement.controller;
 
 import com.property.propertymanagement.dto.PropertyDTO;
+import com.property.propertymanagement.service.PropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
 public class PropertyController {
+
+    @Autowired
+    private PropertyService propertyService;
 
     @GetMapping(path = "/hello")
     public String sayHello(){
@@ -15,6 +21,7 @@ public class PropertyController {
     // Adding property
     @PostMapping("/properties")
     public PropertyDTO saveProperty(@RequestBody PropertyDTO propertyDTO){
+        propertyService.saveProperty(propertyDTO);
         System.out.println(propertyDTO);
         return propertyDTO;
     }
--- property-management/src/main/java/com/property/propertymanagement/service/impl/PropertyServiceImpl.java
+++ property-management/src/main/java/com/property/propertymanagement/service/impl/PropertyServiceImpl.java
@@ -2,7 +2,11 @@ package com.property.propertymanagement.service.impl;
 
 import com.property.propertymanagement.dto.PropertyDTO;
 import com.property.propertymanagement.service.PropertyService;
+import org.springframework.stereotype.Service;
 
+// class to implement the PropertyService
+
+@Service
 public class PropertyServiceImpl implements PropertyService {
 
 
