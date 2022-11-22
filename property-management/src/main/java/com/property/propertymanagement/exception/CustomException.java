129faf2240496e77ee8583a4dc1b6ff8c9240bbe
egyping <fahdkuwait@gmail.com>
2022-11-22 06:16:52 +0300

    AOP started

---
+++ property-management/src/main/java/com/property/propertymanagement/exception/CustomException.java
@@ -0,0 +1,11 @@
+package com.property.propertymanagement.exception;
+
+import java.util.List;
+
+public class CustomException extends Exception {
+
+    // get the ErrorModel here in list style as it may be several error
+    private List<ErrorModel> errors;
+
+    
+}
